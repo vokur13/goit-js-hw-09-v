54cb225647ceaff4309094fe2cf8098654555317
Volodymyr Kurov <103522917+vokur13@users.noreply.github.com>
2022-08-31 14:47:48 +0300

    Initialize gh-pages

---
+++ index.f6113ecb.js
@@ -0,0 +1,2 @@
+var objA={x:1,y:2},objB={y:5,z:7},objC={};for(var key in console.log("objA:",objA),console.log("objB:",objB),objA)if(objA.hasOwnProperty(key)){var element=objA[key];console.log("key:",key),console.log("element:",element)}
+//# sourceMappingURL=index.f6113ecb.js.map
